ae9b6562eb35d1482f97c4912fd465c5b695ec30
Goku216 <sagarpariyar345@gmail.com>
2024-08-16 13:38:22 -0400

    Dark Mode applied

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-white  dark:bg-gray-800">
+      <div className="w-full rounded-lg shadow mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2030{" "}
           <Link to="/" className="hover:underline">
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,21 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import React from 'react';
+import React from "react";
 import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
-
 
 export const AllRoutes = () => {
   return (
-    <>
-    <Routes>
+    <div className="dark:bg-slate-800">
+      <Routes>
         <Route path="" element={<MovieList />} />
-        <Route path="movie/:id" element={<MovieList element={<MovieDetail/>} />} />
+        <Route
+          path="movie/:id"
+          element={<MovieList element={<MovieDetail />} />}
+        />
         <Route path="movies/popular" element={<MovieList />} />
         <Route path="movies/top" element={<MovieList />} />
         <Route path="movies/upcoming" element={<MovieList />} />
         <Route path="search" element={<Search />} />
         <Route path="*" element={<PageNotFound />} />
-
-    </Routes>
-    </>
-  )
-}
+      </Routes>
+    </div>
+  );
+};
