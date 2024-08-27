9be00e31d3d68e0ba030632da9aa26c1919e5812
Goku216 <sagarpariyar345@gmail.com>
2024-08-27 08:53:25 -0400

    Added custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        other: { min: "340px", max: "1260px" },
+      },
+      colors: {
+        darkbg: "#1E293B",
+      },
+    },
   },
   plugins: [],
 };
